cd05e76dd0d93dd4d2ba6db4542f8cfc556b464f
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-10-21 08:25:11 +0300

    Reflect upstream changes in pom.xml to project.clj

--- openpdf/project.clj
+++ openpdf/project.clj
@@ -1,14 +1,14 @@
-(defproject lupapiste/openpdf "1.0.5"
+(defproject lupapiste/openpdf "1.0.6"
   :description "A fork of OpenPDF, which itself is a fork of iText 4.2.1"
   :url "https://github.com/lupapiste/OpenPDF"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/mit-license.php"}
+  :license {:name "GNU General Lesser Public License (LGPL) version 3.0"
+            :url "http://www.gnu.org/licenses/lgpl.html"}
   :scm {:name "git"
         :tag "lupapiste"
         :url "https://github.com/lupapiste/OpenPDF.git"
         :dir "openpdf"}
-  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.54"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.54"]]
+  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.55"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.55"]]
   :java-source-paths ["src/main/java"]
   :source-paths ["src/main/java"]
   :profiles {:uberjar {:aot :all}
